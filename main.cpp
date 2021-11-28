44fb4d72786fdfd4f9c7da4bbd68d9f3e222b486
Justin Ma <jma2@ufl.edu>
2021-11-27 23:50:40 -0500

    Cleaned up main interface

--- main.cpp
+++ main.cpp
@@ -2,6 +2,64 @@
 #include <vector>
 #include "Graph.h"
 
+bool isValidNum(string num);
+void bfsSearch(Graph* g);
+void dijkstras(Graph* g);
+void listPages(Graph* g);
+void readFiles(Graph* g, std::vector<string>& files);
+
+int main() {
+    Graph* g = new Graph;
+
+    std::vector<string> fileNames;
+
+    readFiles(g, fileNames);
+
+    string input = "";
+
+    while (input != "5")
+    {
+        std::cout << "What would you like to do? Select the number that corresponds with your choice:" << std::endl;
+        std::cout << "1. List page links" << std::endl;
+        std::cout << "2. Find the shortest path by pages" << std::endl;
+        std::cout << "3. Find the shortest path by links" << std::endl;
+        std::cout << "4. Insert more data files" << std::endl;
+        std::cout << "5. Stop the program" << std::endl;
+        getline(std::cin, input);
+        std::cout << std::endl;
+
+        char i;
+        if (!input.empty())
+            i= input[0];
+        else
+            i = '0';
+        switch (i)
+        {
+            case '1' :
+                listPages(g);
+                break;
+            case '2' :
+                bfsSearch(g);
+                break;
+            case '3' :
+                dijkstras(g);
+                break;
+            case '4' :
+                delete g;
+                g = new Graph;
+                readFiles(g, fileNames);
+                break;
+            case '5' :
+                break;
+            default:
+                std::cout << "Sorry, your selection was not available." << std::endl;
+                std::cout << "Be sure to only enter a number from the given list" << std::endl;
+                std::cout << std::endl;
+        }
+    }
+    return 0;
+}
+
 bool isValidNum(string num)
 {
     for (int i = 0; i < num.length(); i++)
@@ -20,9 +78,42 @@ bool isValidNum(string num)
     return true;
 }
 
-int main() {
-    Graph g;
+void bfsSearch(Graph* g)
+{
+    string start;
+    std::cout << "Enter the name of the starting page:" << std::endl;
+    getline(std::cin, start);
+    std::cout << std::endl;
+    string dest;
+    std::cout << "Enter the name of the destination page:" << std::endl;
+    getline(std::cin, dest);
+    std::cout << std::endl;
+    g->printBFSPath(start, dest);
+}
 
+void dijkstras(Graph* g)
+{
+    string start;
+    std::cout << "Enter the name of the starting page:" << std::endl;
+    getline(std::cin, start);
+    std::cout << std::endl;
+    string dest;
+    std::cout << "Enter the name of the destination page:" << std::endl;
+    getline(std::cin, dest);
+    std::cout << std::endl;
+    //g->printDijkstrasPath(start, dest);
+}
+
+void listPages(Graph* g)
+{
+    string input;
+    std::cout << "Enter the name of the page to display their links:" << std::endl;
+    getline(std::cin, input);
+    g->printEdges(input);
+}
+
+void readFiles(Graph* g, std::vector<string>& files)
+{
     string fileNum;
     std::cout << "How many files would you like to use to build this graph?" << std::endl;
     getline(std::cin, fileNum);
@@ -37,27 +128,14 @@ int main() {
 
     int numFiles = std::stoi(fileNum);
 
-    std::vector<string> fileNames;
-
     for (int i = 0; i < numFiles; i++)
     {
         string fileName;
         std::cout << "Enter the name of the file you would like to use: " << std::endl;
         getline(std::cin, fileName);
         std::cout << std::endl;
-        fileNames.push_back(fileName);
+        files.push_back(fileName);
     }
 
-    g.inputGraph(fileNames);
-
-    string input = "";
-
-    while (input != "STOP")
-    {
-        std::cout << "Enter the name of the page to display their links, or enter \"STOP\" to end the program: " << std::endl;
-        getline(std::cin, input);
-        if (input != "STOP")
-            g.printEdges(input);
-    }
-    return 0;
+    g->inputGraph(files);
 }
